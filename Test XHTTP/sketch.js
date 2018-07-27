5ba90d6fe8de85d5cb84007ea627f15d46c341e6
greenfIame <greenfiame@users.noreply.github.com>
2018-07-27 14:49:34 +1000

    x

--- Test XHTTP/sketch.js
+++ Test XHTTP/sketch.js
@@ -1,21 +1,25 @@
 function start() {
-	//						https://en.wikipedia.org/w/api.php?action=parse&format=json&page=helium&section=0
+	var u = 	"https://en.wikipedia.org/w/api.php"
+	var data = {
+		action: 'parse',
+		format: 'json',
+		page: 'helium',
+		section: '0',
+		origin: 'https://greenfiame.github.io/Test%20XHTTP/index.html'
+	}
+	var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=helium&section=0"
+
+
 	$.ajax( {
-		url: 'https://en.wikipedia.org/w/api.php?origin=https://greenfiame.github.io/Test%20XHTTP/index.html',
-		data: {
-			action: 'parse',
-			format: 'json',
-			page: 'helium',
-			section: '0',
-			origin: 'https://greenfiame.github.io/Test%20XHTTP/index.html'
-    },
-		dataType: 'json',
-		type: 'GET',
-		headers: { 'Api-User-Agent': 'Example/1.0' },
-		success: function(data) {
-			console.log(data)
-       // do something with data
-    }
+	    url: 'https://en.wikipedia.org/w/api.php',
+	    data: data,
+	    xhrFields: {
+	        withCredentials: true
+	    },
+			headers: { 'Api-User-Agent': 'Example/1.0' },
+	    dataType: 'json'
+	} ).done( function ( data ) {
+		alert(data);
 	} );
 }
 
