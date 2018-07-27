ae9a97cd972d1d89985d7a81b7e5f0be6afda50c
greenfIame <greenfiame@users.noreply.github.com>
2018-07-27 14:02:13 +1000

    x

--- Test XHTTP/sketch.js
+++ Test XHTTP/sketch.js
@@ -1,17 +1,19 @@
 function start() {
 	//https://en.wikipedia.org/w/api.php?action=parse&format=json&page=helium&section=0
+	var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=helium&section=0&origin=https://greenfiame.github.io/Test%20XHTTP/index.html"
+	console.log(url)
 	var xhttp = new XMLHttpRequest()
-		xhttp.onreadystatechange = function() {
-			console.log(["request not initialized", "server connection established", "request received", "processing request", "request finished and response is ready"][this.readyState])
-		}
-	  xhttp.onload = function() {
-	    console.log(this)
-	  }
-		xhttp.onerror = function() {
-			console.log("failed")
-		}
-	  xhttp.open("GET", 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=helium&section=0&origin=https://greenfiame.github.io/Test%20XHTTP/index.html', true)
-	  xhttp.send()
+	xhttp.onreadystatechange = function() {
+		console.log(["request not initialized", "server connection established", "request received", "processing request", "request finished and response is ready"][this.readyState])
+	}
+  xhttp.onload = function() {
+    console.log(this)
+  }
+	xhttp.onerror = function() {
+		console.log("failed")
+	}
+  xhttp.open("GET", url, true)
+  xhttp.send()
 	
 	// $.ajax( {
 	//     url: 'https://en.wikipedia.org/w/api.php',
